d80db1e3b66fb743a81691199b8464aeea9e153f
MykhailoLytvyn <cresedel211194@gmail.com>
2023-05-26 10:35:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [cur, setCur] = useState([])
+  const [rate, setRate] = useState(0);
+  const [wallet, setWallet] = useState('EUR');
+  const _api = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20230519&json'
+
+    useEffect(() => {
+     async function getValue() {
+      try{
+        await fetch(_api).then((response) => response.json()).then((data) => {
+          setCur(data)
+          console.log(data)
+          setRate(data.find(e => e.cc === wallet).rate);
+        }).catch(err => {
+          console.log(`${err}`);
+          setRate('No result')
+        });
+      } catch(e){
+        console.log(e)
+      }
+    }
+      getValue()
+    }, [wallet])
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {rate}
+          <select
+            value={wallet}
+            onChange={e => setWallet(e.target.value)}
+          > 
+            {cur.map( e =>
+              <option value={e.cc}>{e.cc}</option>
+            )}  
+          </select>
       </header>
     </div>
   );
